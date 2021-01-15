27f710cd99fea5bdebe789c0a8a7124bc0700176
Igor Zinar <igorzinar@gmail.com>
2021-01-15 11:07:30 +0200

    5. Style About and Technologies sections, add fonts Inter and Lobster

--- src/components/TechnologiesSection.js
+++ src/components/TechnologiesSection.js
@@ -10,93 +10,147 @@ import nodeJs from '../img/icons/nodejs.png'
 import git from '../img/icons/git.png'
 import github from '../img/icons/github.png'
 import home2 from '../img/home2.png'
+import styledComp from '../img/icons/styledComp.png'
+import reduxLy from '../img/icons/reduxLy.png'
+import npmP from '../img/icons/npmP.png'
+
+// Styles
+import styled from 'styled-components'
+import { About, Description, Image } from '../styles'
 
 function TechnologiesSection() {
   return (
-    <div className="technologies">
-      <div className="description">
+    <Technologies>
+      <Description>
         <h2>
           Only <span>best</span> technologies.
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={vsCode} alt="vsCode" />
               <h3>vsCode</h3>
             </div>
-            <p>Use vsCode like a boss</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={html} alt="html" />
               <h3>HTML5</h3>
             </div>
-            <p>Grate webpages</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={css} alt="css" />
               <h3>CSS3</h3>
             </div>
-            <p>Create mobile responsive pages</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={sass} alt="sass" />
               <h3>SASS</h3>
             </div>
-            <p>------</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={javascript} alt="javascript" />
               <h3>JAVASCRIPT</h3>
             </div>
-            <p>------</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={react} alt="react" />
               <h3>REACT</h3>
             </div>
-            <p>------</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={nodeJs} alt="nodeJs" />
               <h3>NodeJs</h3>
             </div>
-            <p>------</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={git} alt="git" />
               <h3>GIT</h3>
             </div>
-            <p>------</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={github} alt="github" />
               <h3>GitHub</h3>
             </div>
-            <p>------</p>
-          </div>
-        </div>
-      </div>
-      <div className="image">
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={styledComp} alt="styled" />
+              <h3>Styled Components</h3>
+            </div>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={reduxLy} alt="redux" />
+              <h3>Redux</h3>
+            </div>
+          </Card>
+
+          <Card>
+            <div className="icon">
+              <img src={npmP} alt="npm" />
+              <h3 className="npm">NPM</h3>
+            </div>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="" />
-      </div>
-    </div>
+      </Image>
+    </Technologies>
   )
 }
 
+const Technologies = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    /* width: 50%; */
+    padding: 2rem 0 4rem 0;
+  }
+`
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  img {
+    width: 80px;
+    margin-bottom: 2rem;
+  }
+`
+const Card = styled.div`
+  flex-basis: 10rem;
+  margin-bottom: 2rem;
+  .icon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  h3 {
+    font-size: 1.4rem;
+    text-align: center;
+  }
+  h3.npm {
+    padding-top: 2.5rem;
+  }
+`
 export default TechnologiesSection
