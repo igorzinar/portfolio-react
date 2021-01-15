eaf6c15d3a439e60eb621ea5e73940ff2dc6c21b
Igor Zinar <igorzinar@gmail.com>
2021-01-15 05:15:14 +0200

    4. Faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions ? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start</h4>
+        <div className="answer">
+          <p>We start from </p>
+          <p>Analyse the market and find solution </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule </h4>
+        <div className="answer">
+          <p>We start from </p>
+          <p>Analyse the market and find solution </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Difrent Paymont Methods</h4>
+        <div className="answer">
+          <p>We start from </p>
+          <p>Analyse the market and find solution </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What Product do you offer.</h4>
+        <div className="answer">
+          <p>We start from </p>
+          <p>Analyse the market and find solution </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FaqSection
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
 import TechnologiesSection from '../components/TechnologiesSection'
-
+import FaqSection from '../components/FaqSection'
 function AboutMe() {
   return (
     <>
       <AboutSection />
       <TechnologiesSection />
+      <FaqSection />
     </>
   )
 }
