617e6f0e8a667d14f3264111760da2fd1d861a6c
Igor Zinar <igorzinar@gmail.com>
2021-01-15 06:25:17 +0200

    start StyledComponent add GlobalStyle component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+// Global Style
+import GlobalStyle from './components/GlobalStyle'
+// Import Pages
 import AboutMe from './pages/AboutMe'
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <AboutMe />
     </div>
   )
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,64 @@
 import React from 'react'
 import home1 from '../img/home1.png'
+import styled from 'styled-components'
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>I work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               cool <span> web app </span>
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>for fun</h2>
-          </div>
+          </Hide>
         </div>
         <p> Contact with me for details about work and more... </p>
         <button>Contact Me</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="home1" />
-      </div>
-    </div>
+      </Image>
+    </About>
   )
 }
 
+//Styled Component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #fff;
+`
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+
+  h2 {
+    font-weight: lighter;
+  }
+`
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`
+
+const Hide = styled.div`
+  overflow: hidden;
+`
 export default AboutSection
